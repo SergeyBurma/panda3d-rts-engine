bc744a9b9b519d7fa4a4d35efe7ee0589a81a9fc
phocus <phocus@0b918e4b-b81a-234b-e25a-7d2d2d63f29b>
2010-05-06 21:25:35 +0000

    Added ralph point click

--- main.py
+++ main.py
@@ -13,6 +13,9 @@ import math
 class RTSEngine(DirectObject):
     
     def __init__(self):
+        from pandac.PandaModules import loadPrcFileData
+        loadPrcFileData("", "want-directtools #t")
+        loadPrcFileData("", "want-tk #t")
         self.camHandler = None
         self.environ = None
         self.isMoving = False
@@ -21,16 +24,15 @@ class RTSEngine(DirectObject):
         self.picked = None
         self.selected = None
         
-        self.setupMouseCollision()
 
         self.setupEnvironment()
         self.setupCamera()
         self.setupActors()
         self.setupLighting()
-        
+        self.setupMouseCollision()
         self.accept("mouse1",self.selectObject) 
-
         taskMgr.add(self.handleCursor,"HandleMouse")
+        taskMgr.add(self.handleRalphCollision,"HandleRalphCollision")
         
         
    
@@ -40,13 +42,33 @@ class RTSEngine(DirectObject):
         run()
   
     
+
     def setupMouseCollision(self):
+        
+        
+        self.ralphGroundRay = CollisionRay()
+        self.ralphGroundRay.setOrigin(0,0,1000)
+        self.ralphGroundRay.setDirection(0,0,-1)
+        self.ralphGroundCol = CollisionNode('ralphRay')
+        self.ralphGroundCol.addSolid(self.ralphGroundRay)
+        self.ralphGroundCol.setFromCollideMask(BitMask32.bit(0))
+        self.ralphGroundCol.setIntoCollideMask(BitMask32.allOff())
+        self.ralphGroundColNp = self.ralph.attachNewNode(self.ralphGroundCol)
+        self.ralphGroundHandler = CollisionHandlerQueue()
+        self.trav.addCollider(self.ralphGroundColNp, self.ralphGroundHandler)
+ 
+        
+        
         pickerNode = CollisionNode('mouseRay')
-        pickerNP = camera.attachNewNode(pickerNode)
+        self.pickerNP = camera.attachNewNode(pickerNode)
         pickerNode.setFromCollideMask(GeomNode.getDefaultCollideMask())
         self.pickerRay = CollisionRay()
         pickerNode.addSolid(self.pickerRay)
-        self.trav.addCollider(pickerNP ,self.cHandler)
+        self.trav.addCollider(self.pickerNP ,self.cHandler)
+        self.trav.showCollisions(render)
+        z=0
+        self.plane = Plane(Vec3(0, 0, 1), Point3(0, 0, z)) 
+
         
     def setupCamera(self):
         self.camHandler = CameraHandler()
@@ -55,7 +77,7 @@ class RTSEngine(DirectObject):
     def setupActors(self):
         # Create the main character, Ralph
         ralphStartPos = self.environ.find("**/start_point").getPos()
-        self.ralph = Actor.Actor("models/ralph",{"run":"models/ralph-run","walk":"models/ralph-walk"})
+        self.ralph = Actor.Actor("/home/focus/svn/panda3d-rts-engine/models/ralph",{"run":"models/ralph-run","walk":"models/ralph-walk"})
         self.ralph.reparentTo(render)
         self.ralph.setScale(.2)
         self.ralph.setPos(1,1,1)
@@ -64,6 +86,23 @@ class RTSEngine(DirectObject):
         self.floater = NodePath(PandaNode("floater"))
         self.floater.setPos(self.ralph.getPos())
         self.floater.reparentTo(render)
+        
+        self.pointer = loader.loadModel("/home/focus/svn/panda3d-rts-engine/models/box")
+        self.pointer.setPos(5,5,1)
+        self.pointer.setColor(1,0,0)
+        self.pointer.setScale(.5)
+        self.pointer.reparentTo(render)
+        
+        
+        cmfg = CardMaker('fg') 
+        cmfg.setFrame(0, 1, -1, 0) 
+        self.bar = render2d.attachNewNode(cmfg.generate())
+        self.bar.setColor(0,1,0,.5)
+        self.bar.setTransparency(1)
+        
+
+        
+    
         
         
         
@@ -85,34 +124,65 @@ class RTSEngine(DirectObject):
         alight.setColor(VBase4(0.5,0.5,0.5,0.5))
         self.alnp = self.ralph.attachNewNode(alight)
         
+        self.floater.attachNewNode(alight)
+        self.floater.setLight(self.alnp)
+        
+        
         
     def setupEnvironment(self):
-        self.environ = loader.loadModel("models/world") 
+        self.environ = loader.loadModel("/home/focus/svn/panda3d-rts-engine/models/world") 
         self.environ.reparentTo(render)
         self.environ.setScale(0.25,0.25,0.25) 
         self.environ.setPos(0,0,0)
         
+    def mover(self):
+        return
+        
+        
     def selectObject(self):
         if(base.mouseWatcherNode.hasMouse()):
-            mpos = mpos = base.mouseWatcherNode.getMouse()
-            if not (self.picked == None):
-                if (self.selected == None):
-                    return
-                self.selected =  self.picked
+            
+            if(selected == None):
                 return
-            else:
-                z = self.environ.getZ(mpos.getPos())
-                self.floater.setPos(mpos.getX(),mpos.getY())
-                
-                
+            
+            mpos = base.mouseWatcherNode.getMouse() 
+            pos3d = Point3() 
+            nearPoint = Point3() 
+            farPoint = Point3() 
+            base.camLens.extrude(mpos, nearPoint, farPoint) 
+            if self.plane.intersectsLine(pos3d, 
+                render.getRelativePoint(camera, nearPoint), 
+                render.getRelativePoint(camera, farPoint)):
+                self.floater.setPos(render, pos3d.getX(),pos3d.getY(),self.pickerNP.getZ())
+            
         
+        
+    def handleRalphCollision(self,task):
+        
+        entries = []
+        for i in range(self.ralphGroundHandler.getNumEntries()):
+            entry = self.ralphGroundHandler.getEntry(i)
+            entries.append(entry)
+        entries.sort(lambda x,y: cmp(y.getSurfacePoint(render).getZ(),
+                                     x.getSurfacePoint(render).getZ()))
+        if (len(entries)>0) and (entries[0].getIntoNode().getName() == "terrain"):
+            self.ralph.setZ(entries[0].getSurfacePoint(render).getZ())
+        else:
+            self.ralph.setPos(startpos)
+
+        return task.cont
+    
+                            
+                
+                
     def handleCursor(self,task):
-        if(self.isMoving):
-            self.ralph.loop("run")
         if base.mouseWatcherNode.hasMouse(): 
             # We're going to use the mouse, so we have to make sure it's in the game window. If it's not and we try to use it, we'll get 
             # a crash error. 
             mpos = base.mouseWatcherNode.getMouse()
+            pos3d = Point3()
+            nearPoint = Point3()
+            farPoint = Point3()
             self.pickerRay.setFromLens(base.camNode,mpos.getX(),mpos.getY())
             self.trav.traverse(render)
             
@@ -128,9 +198,12 @@ class RTSEngine(DirectObject):
                         if not (self.picked == None):
                             self.picked.clearLight(self.alnp)
                             self.picked= None
+                    
 
                 except:
                     pass
+                
+
                 
         return task.cont
 
